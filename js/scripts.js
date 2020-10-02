2fd2e20a094b89f0afda9ba627271f176ea3f737
NataLee38 <light3475house@gmail.com>
2020-10-02 10:23:27 +0300

    changed dialog

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,4 @@
 $(document).ready(function(){
-
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: [ '', ' ' ],
-
-        responsive:{
-            0:{
-                items:1
-            },
-
-            1000:{
-                items:1
-            }
-        }
-    });
-});
-$( function() {
     $( "#dialog" ).dialog({
         autoOpen: false,
         show: {
@@ -33,4 +14,22 @@ $( function() {
     $( "#opener" ).on( "click", function() {
         $( "#dialog" ).dialog( "open" );
     });
-} );
+
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+
+    responsive:{
+        0:{
+            items:1
+        },
+
+        1000:{
+            items:1
+        }
+    }
+});
