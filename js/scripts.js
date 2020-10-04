6992e34e2dcf037a0ea42ee7ccadf8f71703167c
NataLee38 <light3475house@gmail.com>
2020-10-04 20:31:44 +0300

    added burgermenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
+
 $(document).ready(function(){
-    $( function() {
-        $( "#dialog" ).dialog({
-            autoOpen: false,
-            show: {
-                effect: "blind",
-                duration: 1000
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
             },
-            hide: {
-                effect: "explode",
-                duration: 1000
+
+            1000:{
+                items:1
             }
-        });
-
-        $( "#opener" ).on( "click", function() {
-            $( "#dialog" ).dialog( "open" );
-        });
-    } );
-});
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    nav:true,
-    navText: [ '', ' ' ],
-
-    responsive:{
-        0:{
-            items:1
-        },
-
-        1000:{
-            items:1
         }
-    }
+    });
+
 });
+
+
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('nav .menu-btn .line--1');
+var lineTwo = document.querySelector('nav .menu-btn .line--2');
+var lineThree = document.querySelector('nav .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
